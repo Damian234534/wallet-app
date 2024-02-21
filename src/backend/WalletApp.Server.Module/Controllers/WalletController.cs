5d69012bb7bf9cc0e61663611a3b65037df89327
Damian234534 <damian234534@gmail.com>
2024-02-21 12:18:17 +0100

    Create WalletController.cs

---
+++ src/backend/WalletApp.Server.Module/Controllers/WalletController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Mvc;
+using WalletApp.Server.Core.Models;
+
+namespace WalletApp.Server.Module.Controllers
+{
+    [ApiController]
+    [Route("api/wallet")]
+    public class WalletController : ControllerBase
+    {
+        private static List<Wallet> wallets = new List<Wallet>();
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult CreateWallet()
+        {
+
+            wallets.Add(new Wallet(){
+                Id = Guid.NewGuid(),
+                Name = Guid.NewGuid().ToString()
+            });
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("balance")]
+        public IActionResult GetBalance()
+        {
+            return Ok(wallets);
+        }
+    }
+}
