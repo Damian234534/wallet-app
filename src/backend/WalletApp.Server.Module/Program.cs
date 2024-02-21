f99aa9e97037dde1d2335c1a8bba45e0a2f88405
Damian234534 <damian234534@gmail.com>
2024-02-21 12:12:58 +0100

    Add configuration for backend

---
+++ src/backend/WalletApp.Server.Module/Program.cs
@@ -0,0 +1,31 @@
+﻿using WalletApp.Server.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+using Serilog;
+using Microsoft.AspNetCore.Builder;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("logs/log.txt",
+    rollingInterval: RollingInterval.Day,
+    rollOnFileSizeLimit: true)
+    .CreateLogger();
+
+var configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.Bind(configuration);
+builder.Services.Configure<ConfigSettings>(builder.Configuration);
+
+builder.Logging.AddSerilog();
+builder.Services.AddLogging();
+
+builder.WebHost.UseUrls(urls: builder.Configuration[nameof(ConfigSettings.WEB_HOST_URL)]!);
+
+var app = builder.Build();
+
+app.Run();
